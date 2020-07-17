e0ead469cb6ff6a2b73a144f5c74307609ecb32e
Tommy Mabrier <tommy.mabrier@kubity.com>
2020-07-17 17:20:44 +0200

    Added new projects

--- sketch-airtable-sync.sketchplugin/Contents/Sketch/sync-layers.js
+++ sketch-airtable-sync.sketchplugin/Contents/Sketch/sync-layers.js
@@ -11087,9 +11087,13 @@ function getCleanArtboardName(name) {
 module.exports = {
   APIKey: 'keyf4awab19Xtmlye',
   bases: {
+    account: 'appzlofnOZISt45v4',
     archiklip: 'appah63sWZZp4m8Na',
     kubity: 'appkfJduoblSphVdp',
-    rvt2skp: 'appegmk9i1AWF5vwg'
+    rvt2skp: 'appegmk9i1AWF5vwg',
+    webplayer: 'appJlbaWsz4f1e5xS',
+    '3d2vr': 'appV0J8bqbfFhsn1C',
+    mirror_page: 'appRXq7LG9wKyVQAf'
   },
   table: 'YOUR-TABLE-NAME',
   view: 'Grid view'
--- src/secret.js
+++ src/secret.js
@@ -1,9 +1,13 @@
 module.exports = {
 	APIKey: 'keyf4awab19Xtmlye',
 	bases: {
+		account: 'appzlofnOZISt45v4',
 		archiklip: 'appah63sWZZp4m8Na',
 		kubity: 'appkfJduoblSphVdp',
 		rvt2skp: 'appegmk9i1AWF5vwg',
+		webplayer: 'appJlbaWsz4f1e5xS',
+		'3d2vr': 'appV0J8bqbfFhsn1C',
+		mirror_page: 'appRXq7LG9wKyVQAf',
 	},
 	table: 'YOUR-TABLE-NAME',
 	view: 'Grid view'
